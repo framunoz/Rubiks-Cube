38e6ecea761da5dca91a9b6d612db33ee32dc25d
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-07-23 00:54:09 -0400

    Initial commit. Setting initial configuration.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pathlib
 from pkg_resources import parse_requirements
 from setuptools import find_packages, setup
 
-LIBRARY_NAME = "library"  # Rename according to te "library" folder
+LIBRARY_NAME = "rubiks_cube"  # Rename according to the library folder
 
 # List of requirements
 with pathlib.Path('requirements.txt').open() as requirements_txt:
@@ -15,11 +15,11 @@ setup(
     name=LIBRARY_NAME,
     packages=find_packages(include=[LIBRARY_NAME]),
     version="0.1.0",
-    description="Description",
-    author="Author",
+    description="A library that allows to simulate a Rubik's Cube for research purposes.",
+    author="Francisco Muñoz",
     license="MIT",
     install_requires=install_requires,
     setup_requires=["pytest-runner"],
-    tests_requires=["pytest==4.4.1"],
+    tests_requires=["pytest"],
     test_suite="tests",
 )
