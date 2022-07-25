80b46dbf06dd89a083191f9c62580ec4bd106ecb
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-07-24 21:32:01 -0400

    feat: Implemented the colors in the text. It matches with its colors.

--- rubiks_cube/utils.py
+++ rubiks_cube/utils.py
@@ -1,23 +1,34 @@
 import enum
 
+from colorama import Fore, Style
+
+_COLORS = {
+    "green": Fore.GREEN,
+    "red": Fore.RED,
+    "yellow": Fore.YELLOW,
+    "orange": Style.BRIGHT + Fore.RED,
+    "blue": Fore.BLUE,
+    "white": Style.DIM + Fore.WHITE
+}
+
 
 class Color(enum.Enum):
     """
     Enumerator for the colors of the faces of a Rubik's cube.
     """
-    GREEN = enum.auto()
-    RED = enum.auto()
-    YELLOW = enum.auto()
-    ORANGE = enum.auto()
-    BLUE = enum.auto()
-    WHITE = enum.auto()
+    GREEN = "green"
+    RED = "red"
+    YELLOW = "yellow"
+    ORANGE = "orange"
+    BLUE = "blue"
+    WHITE = "white"
 
     def __repr__(self):
-        return self.name[0]
+        return _COLORS[self.value] + self.name[0] + Style.RESET_ALL
 
 
 class Direction(enum.Enum):
-    U = enum.auto()
-    R = enum.auto()
-    D = enum.auto()
-    L = enum.auto()
+    U = 0
+    R = 1
+    D = 2
+    L = 3
