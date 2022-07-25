53693235f0fa2bf3b7cacbeeba289f3b5e940090
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-07-24 22:54:06 -0400

    debug: Implemented and debugged the rotation method.

--- rubiks_cube/faces.py
+++ rubiks_cube/faces.py
@@ -1,13 +1,17 @@
 from __future__ import annotations
 
-from typing import Tuple, Optional
+from typing import Tuple, List, Union
+
 import numpy as np
 
 from rubiks_cube.utils import Color
 from rubiks_cube.utils import Direction as Direc
 
+Slice = Union[slice, int]
+TupleSlice = Tuple[Slice, Slice]
 
-def generate_slice_from_direction(direction):
+
+def generate_slice(direction) -> TupleSlice:
     all_ = slice(None, None)
     if direction == Direc.U:
         return 0, all_
@@ -17,24 +21,45 @@ def generate_slice_from_direction(direction):
         return -1, all_
     elif direction == Direc.L:
         return all_, 0
-    return None
+    return all_, all_
 
 
 class Face:
-    def __init__(self, color: Color, shape: Tuple[int, int]):
-        self.color: Color = color
+    def __init__(self, color: Color | str, shape: Tuple[int, int]):
+        self.color: Color = Color(color)
         self.shape: Tuple[int, int] = shape
 
-        self.central_face: np.ndarray[Color] = np.tile(color, shape)
-        self.up: Optional[Face] = None
-        self.right: Optional[Face] = None
-        self.down: Optional[Face] = None
-        self.left: Optional[Face] = None
-        self._slice_list = None
+        # Central Face
+        matrix = np.ones(self.shape, dtype=object)
+        matrix[:, :] = self.color
+        self.central_face: np.ndarray[Color] = matrix
+
+        # Faces
+        self.up: Face | None = None
+        self.right: Face | None = None
+        self.down: Face | None = None
+        self.left: Face | None = None
+
+        self._slice_list: List[TupleSlice] | None = None
 
     @property
     def cf(self) -> np.ndarray[Color]:
         return self.central_face
+
+    @property
+    def faces(self) -> List[Face]:
+        return [self.up, self.right, self.down, self.left]
+
+    @property
+    def pieces(self):
+        u_s, r_s, d_s, l_s = self._slice_list
+        to_return: List[np.ndarray] = [self.up.cf[u_s], self.right.cf[r_s], self.down.cf[d_s], self.left.cf[l_s]]
+        return [arr.copy() for arr in to_return]
+
+    @pieces.setter
+    def pieces(self, to_set):
+        u_s, r_s, d_s, l_s = self._slice_list
+        self.up.cf[u_s], self.right.cf[r_s], self.down.cf[d_s], self.left.cf[l_s] = to_set
 
     def __repr__(self):
         return self.central_face.__repr__()
@@ -42,25 +67,12 @@ class Face:
     def add_faces(self, up_tuple, right_tuple, down_tuple, left_tuple):
         # TODO: Agregar algo para que valide que las caras realmente se ajusten? onda que
         #  si la cara central es de (2, 2) que el de la izquierda sea de (2, 100) y no de (100, 100).
-        (up, up_s), (right, right_s), (down, down_s), (left, left_s) = up_tuple, right_tuple, down_tuple, left_tuple
+        (up, up_d), (right, right_d), (down, down_d), (left, left_d) = up_tuple, right_tuple, down_tuple, left_tuple
         self.up, self.right, self.down, self.left = up, right, down, left
-        self._slice_list = up_s, right_s, down_s, left_s
+        self._slice_list = [generate_slice(Direc(s)) for s in [up_d, right_d, down_d, left_d]]
 
-    def rotate_once(self):
+    def rotate(self, times: int):
+        times = times % 4
         # TODO: Agregar validadores para chequear que el movimiento se puede hacer (?)
-        up_s, right_s, down_s, left_s = self._slice_list
-        self.up.cf[up_s], self.right.cf[right_s], self.down.cf[down_s], self.left.cf[left_s] = \
-            self.left.cf[left_s], self.up.cf[up_s], self.right.cf[right_s], self.down.cf[down_s]
-
-    def rotate_inverse(self):
-        # TODO: Agregar validadores para chequear que el movimiento se puede hacer (?)
-        up_s, right_s, down_s, left_s = self._slice_list
-        self.up.cf[up_s], self.right.cf[right_s], self.down.cf[down_s], self.left.cf[left_s] = \
-            self.right.cf[right_s], self.down.cf[down_s], self.left.cf[left_s], self.up.cf[up_s]
-
-    def rotate_twice(self):
-        # TODO: Agregar validadores para chequear que el movimiento se puede hacer (?)
-        up_s, right_s, down_s, left_s = self._slice_list
-        self.up.cf[up_s], self.right.cf[right_s], self.down.cf[down_s], self.left.cf[left_s] = \
-            self.down.cf[down_s], self.left.cf[left_s], self.up.cf[up_s], self.right.cf[right_s]
-
+        self.pieces = np.roll(self.pieces, times)
+        self.central_face = np.rot90(self.central_face, times)
--- tests/test_faces.py
+++ tests/test_faces.py
@@ -11,10 +11,11 @@ class TestFace(TestCase):
     def setUp(self) -> None:
         self.face = Face(Color.RED, (3, 2))
         self.up = Face(Color.BLUE, (2, 2))
-        self.down = Face(Color.GREEN, (2, 2))
         self.right = Face(Color.ORANGE, (3, 2))
-        self.left = Face(Color.ORANGE, (3, 2))
-        self.faces = [self.up, self.down, self.right, self.left]
+        self.down = Face(Color.GREEN, (2, 2))
+        self.left = Face(Color.YELLOW, (3, 2))
+        self.faces = [(self.up, 2), (self.right, 3), (self.down, 0), (self.left, 1)]
+        self.face.add_faces(*self.faces)
 
     def test_instance_well_created(self):
         r = Color.RED
@@ -24,9 +25,6 @@ class TestFace(TestCase):
             "The face has not been created correctly.")
 
     def test_add_faces(self):
-        self.face.add_faces(
-            up_tuple=(self.up, 2), right_tuple=(self.right, 3), down_tuple=(self.down, 0), left_tuple=(self.left, 1)
-        )
         np.testing.assert_array_equal(
             self.face.up.central_face, self.up.central_face,
             "The 'up' face is not correct."
@@ -43,6 +41,38 @@ class TestFace(TestCase):
             self.face.left.central_face, self.left.central_face,
             "The 'left' face is not correct."
         )
+
+    def test_rotate(self):
+        # Change a corner
+        self.face.central_face[0, 0] = Color.WHITE
+        # List of news faces
+        g, r, y, o, b, w = Color.GREEN, Color.RED, Color.YELLOW, Color.ORANGE, Color.BLUE, Color.WHITE
+        up = np.array([
+            [b, b],
+            [g, g]
+        ])
+        right = np.array([
+            [y, o],
+            [y, o],
+            [y, o],
+        ])
+        expected_list = [up, right, up, right]
+        central = np.array([
+            [r, r],
+            [r, r],
+            [r, w],
+        ])
+        # Do a rotation
+        self.face.rotate(2)
+        np.testing.assert_array_equal(
+            self.face.cf, central,
+            "The 'central' face does not rotate correctly."
+        )
+        for res, exp in zip(self.face.faces, expected_list):
+            np.testing.assert_array_equal(
+                res.cf, exp,
+                "A face does not rotate correctly."
+            )
 
 
 if __name__ == '__main__':
