296de42df3746b0c60aab15fa1bb330ccc662044
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-07-31 22:39:26 -0400

    tests: Added the test for the movements for a 3x2x1 Rubik's Cube.

---
+++ tests/test_movements.py
@@ -0,0 +1,92 @@
+import unittest
+
+from rubiks_cube.cube import RubikCube
+from rubiks_cube.faces import Face
+from rubiks_cube.movements import CubeMove
+from rubiks_cube.utils import R, Y, G, W, B
+from tests.test_base import TestBase
+
+
+class TestBaseMove(TestBase):
+    def setUp(self) -> None:
+        super(TestBaseMove, self).setUp()
+        self.rc = RubikCube(*self.faces)
+
+    def _test_move_the_cube(self, move: CubeMove, expected: RubikCube):
+        self.rc.make_a_move(move)
+        self.assertEqual(
+            self.rc, expected,
+            f"'{repr(move)}' movement does not works."
+        )
+
+
+class TestR2(TestBaseMove):
+    def test_move_the_cube(self):
+        expected = RubikCube(
+            Face([[R, Y]]),
+            Face([[R], [G], [W]]),
+            Face([[G, G],
+                  [R, B],
+                  [W, R]]),
+            Face([[Y], [R], [B]]),
+            Face([[B, B],
+                  [W, Y],
+                  [Y, W]]),
+            Face([[W, B]])
+        )
+        self._test_move_the_cube(CubeMove.R2, expected)
+
+
+class TestL2(TestBaseMove):
+    def test_move_the_cube(self):
+        expected = RubikCube(
+            Face([[W, B]]),
+            Face([[W], [G], [R]]),
+            Face([[W, Y],
+                  [Y, W],
+                  [B, B]]),
+            Face([[B], [R], [Y]]),
+            Face([[R, W],
+                  [B, R],
+                  [G, G]]),
+            Face([[R, Y]])
+        )
+        self._test_move_the_cube(CubeMove.L2, expected)
+
+
+class TestU2(TestBaseMove):
+    def test_move_the_cube(self):
+        expected = RubikCube(
+            Face([[B, R]]),
+            Face([[B], [G], [W]]),
+            Face([[R, B],
+                  [R, W],
+                  [W, B]]),
+            Face([[R], [R], [Y]]),
+            Face([[G, Y],
+                  [B, Y],
+                  [G, W]]),
+            Face([[W, Y]])
+        )
+        self._test_move_the_cube(CubeMove.U2, expected)
+
+
+class TestD2(TestBaseMove):
+    def test_move_the_cube(self):
+        expected = RubikCube(
+            Face([[R, B]]),
+            Face([[R], [G], [Y]]),
+            Face([[G, Y],
+                  [R, W],
+                  [G, W]]),
+            Face([[B], [R], [W]]),
+            Face([[R, B],
+                  [B, Y],
+                  [W, B]]),
+            Face([[Y, W]])
+        )
+        self._test_move_the_cube(CubeMove.D2, expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
