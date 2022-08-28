0a5606c116e637a6aee33b265be9267988a7c2f3
Francisco Muñoz <femunoz@dim.uchile.cl>
2022-08-28 14:04:12 -0400

    doc: Actualised the documentation.

--- example.py
+++ example.py
@@ -3,10 +3,8 @@ from matplotlib import pyplot as plt
 
 from rubiks_cube.cube import RubikCube
 from rubiks_cube.graph import make_graph, generate_file
-from rubiks_cube.movements import CubeMove
+from rubiks_cube.movements import CubeMove as CM
 from rubiks_cube.utils import Color
-
-CM = CubeMove
 
 
 def main():
@@ -14,18 +12,18 @@ def main():
 
     rc = RubikCube.from_dims(
         (3, 3, 3),
-        {CubeMove.R2, CubeMove.L2, CubeMove.U2, CubeMove.D2}
+        {CM.R2, CM.L2, CM.U2, CM.D2}
     )
     print(rc, end="\n\n")
     rc = rc.make_movements("L2 U2")
     print("\n", rc, "\n", sep="")
     # list_of_movements = [
-    #     CubeMove.U, CubeMove.R, CubeMove.D, CubeMove.L,
-    #     CubeMove.D, CubeMove.R, CubeMove.D, CubeMove.R
+    #     CM.U, CM.R, CM.D, CM.L,
+    #     CM.D, CM.R, CM.D, CM.R
     # ]
     list_of_movements = [
-        CubeMove.U2, CubeMove.R2, CubeMove.D2, CubeMove.L2,
-        CubeMove.D2, CubeMove.R2, CubeMove.D2, CubeMove.R2
+        CM.U2, CM.R2, CM.D2, CM.L2,
+        CM.D2, CM.R2, CM.D2, CM.R2
     ]
     for m in list_of_movements:
         print(f"{m = }")
@@ -36,14 +34,14 @@ def main():
     for f in rc.faces:
         print(f, end="\n\n")
 
-    print(CubeMove.R)
+    print(CM.R)
 
     print(hash(Color.BLUE), hash(Color.BLUE))
     print(hash("blue"), hash("blue"))
 
 
 def main2():
-    g = make_graph((3, 2, 1), {CM.R2, CM.U2, CM.D2})
+    g: nx.Graph = make_graph((3, 2, 1), {CM.R2, CM.D2, CM.U2})
     generate_file(g)
     for i, rc in enumerate(g.nodes):
         print(f"Estado {i = }")
@@ -53,13 +51,11 @@ def main2():
         neighbors = list(g[rc].keys())
         neighbors.sort(key=lambda x: hash(x))
         print(neighbors)
-    options = {
-        'node_color': 'red',
-        'node_size': 50,
-        'edge_color': 'blue',
-        'width': 3,
-    }
-    nx.draw_kamada_kawai(g, **options)
+    nx.draw_kamada_kawai(
+        g,
+        node_color="red", node_size=50,
+        edge_color="blue", width=3
+    )
     plt.show()
 
 
